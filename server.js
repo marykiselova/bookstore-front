53b37a8c8694dc8e08603f2f9f4b1dc709cea4bd
marykiselova <marykiselova@gmail.com>
2018-07-04 01:38:34 +0300

    change index.html location

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/bookstore-front'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist//bookstore-front/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bookstore-front/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
